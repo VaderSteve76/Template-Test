233e073b9992ee5b7df5f759c306839a6bcf54db
Steve Alverson <alverson.s@yahoo.com>
2020-10-22 15:04:31 -0400

    Getting quotes array in console

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('testing');
+let apiQuotes = [];
+
+// Get Quotes from api
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const res = await fetch(apiUrl);
+    apiQuotes = await res.json();
+    newQuote();
+  } catch (err) {
+    console.log("Whoops, no quote", err);
+  }
+}
+
+// On Load
+getQuotes();
