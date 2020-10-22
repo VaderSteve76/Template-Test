40e8b4cd61b28d3588da1fbea65b8f365c356901
Steve Alverson <alverson.s@yahoo.com>
2020-10-22 15:46:43 -0400

    Added loader with functionality

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote 
 function newQuote() {
+  loading();
   // Pick random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   quoteText.textContent = quote.text;
@@ -25,11 +39,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // Set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes from api
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const res = await fetch(apiUrl);
